c4eae35898bcb996201c099ef5f44a59ba958b70
Denis793 <denisshevchenko109@gmail.com>
2023-08-26 22:31:47 +0200

    deleted folder reserve

--- app/js/reserve/main-2.js
+++
@@ -1,9 +0,0 @@
-// Імпорт модулів
-import authorize from './authorize.js';
-import listZone from './listZone.js';
-import showMessage from './message';
-
-// Виклики функцій з імпортованих модулів
-authorize();
-listZone();
-showMessage();
