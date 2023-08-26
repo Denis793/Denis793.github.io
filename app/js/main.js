3c36fcce970cee53a547d55f351a4ac40db08ae3
Denis793 <denisshevchenko109@gmail.com>
2023-08-26 23:13:30 +0200

    update all files

--- app/js/main.js
+++ app/js/main.js
@@ -1,70 +1,52 @@
+// Функція для відображення повідомлень
+function showMessage(message, isSuccess) {
+	const messagesContainer = document.querySelector('.messages');
+	const messageElement = document.createElement('div');
+	messageElement.classList.add('message', isSuccess ? 'message--success' : 'message--error');
+	messageElement.textContent = message;
+	messagesContainer.appendChild(messageElement);
+
+	// Видалення повідомлення через 5 секунд
+	setTimeout(() => {
+		messageElement.remove();
+	}, 5000);
+}
+
 // Функція авторизації
-function authorize() {
-	const authButton = document.getElementById('authButton');
-	const tokenInput = document.getElementById('tokenInput');
+function authorize(token) {
+	const apiUrl = 'https://storage.blazingcdn.net/v3';
 
-	authButton.addEventListener('click', async () => {
-		const token = tokenInput.value;
-
-		try {
-			const response = await fetch('https://storage.blazingcdn.net/v3', {
-				headers: {
-					'Authorization': `Bearer ${token}`
-				}
-			});
-
+	// Виклик API для авторизації
+	fetch(apiUrl, {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	})
+		.then(response => {
 			if (response.ok) {
 				showMessage('Authorization successful', true);
 			} else {
 				showMessage('Authorization failed', false);
 			}
-		} catch (error) {
-			showMessage('An error occurred', false);
-		}
-	});
-}
-
-// Функція відображення списку CDN зон
-function listZone() {
-	const cdnZoneList = document.getElementById('cdnZoneList');
-
-	const zones = ['Zone 1', 'Zone 2', 'Zone 3'];
-
-	zones.forEach(zone => {
-		const zoneItem = document.createElement('li');
-		zoneItem.innerHTML = `${zone} <button class="delete-button button">Delete</button>`;
-
-		const deleteButton = zoneItem.querySelector('.delete-button');
-		deleteButton.addEventListener('click', () => {
-			deleteZone(zone);
+		})
+		.catch(error => {
+			showMessage('An error occurred during authorization', false);
 		});
-
-		cdnZoneList.appendChild(zoneItem);
-	});
 }
 
-// Функція відображення повідомлення
-function showMessage(message, isSuccess) {
-	const messagesSection = document.querySelector('.messages');
-	const messageElement = document.createElement('div');
-	const messageClass = isSuccess ? 'success' : 'error';
+// Отримання елемента input для token
+const tokenInput = document.getElementById('token');
 
-	messageElement.classList.add('message', messageClass);
-	messageElement.textContent = message;
+// Відправка форми авторизації
+document.getElementById('authForm').addEventListener('submit', function(event) {
+	event.preventDefault();
+	const token = tokenInput.value;
+	if (token) {
+		authorize(token);
+	} else {
+		showMessage('Please enter a token', false);
+	}
+});
 
-	messagesSection.appendChild(messageElement);
-
-	setTimeout(() => {
-		messagesSection.removeChild(messageElement);
-	}, 5000);
-}
-
-// Функція видалення CDN зони (потребує реалізації з використанням API)
-function deleteZone(zone) {
-	// Реалізація видалення зони
-	showMessage(`Zone "${zone}" deleted successfully`, true);
-}
-
-// Виклики функцій
-authorize();
-listZone();
+// Ваші інші функції та код тут
