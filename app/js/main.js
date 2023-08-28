5be2677ab7e06f6a823d597df58aaf8dfc952cbb
Denis793 <denisshevchenko109@gmail.com>
2023-08-28 17:01:21 +0200

    new update js

--- app/js/main.js
+++ app/js/main.js
@@ -1,61 +1,79 @@
-document.addEventListener('DOMContentLoaded', () => {
-	// Функція для виводу повідомлення на певний проміжок часу
-	const showMessage = (message, color = 'black') => {
-		const messagesSection = document.querySelector('.messages');
-		const messageElement = document.createElement('p');
-		messageElement.textContent = message;
+document.addEventListener("DOMContentLoaded", () => {
+	const authForm = document.getElementById("authForm");
+	const tokenInput = document.getElementById("token");
+	const messagesSection = document.querySelector(".messages");
+
+	const showMessage = (message, color) => {
+		const messageElement = document.createElement("p");
 		messageElement.style.color = color;
+		messageElement.textContent = message;
 		messagesSection.appendChild(messageElement);
 		setTimeout(() => {
 			messageElement.remove();
 		}, 7000);
 	};
 
-	// Підключення до серверу blazingcdn.com
-	fetch('https://storage.blazingcdn.net/v3')
-		.then(response => {
-			if (response.ok) {
-				showMessage('Під\'єднання до серверу успішне', 'green');
-			} else {
-				showMessage('Помилка під\'єднання до серверу', 'red');
-			}
-		})
-		.catch(error => {
-			showMessage('Помилка під\'єднання до серверу', 'red');
-		});
+	const connectToServer = async () => {
+		try {
+			await fetch("https://storage.blazingcdn.net/v3", {
+				method: "GET",
+			});
+			showMessage("Під'єднання до серверу успішне", "green");
+		} catch (error) {
+			showMessage("Помилка під'єднання до серверу", "red");
+		}
+	};
 
-	// Перевірка токену
-	const checkToken = token => {
+	const checkTokenValidity = async (token) => {
 		if (!token) {
-			showMessage('Відсутній токен', 'red');
+			showMessage("Відсутній токен", "red");
 			return;
 		}
 
-		// Виконайте перевірку токену тут і виведіть повідомлення відповідно
+		try {
+			const response = await fetch("https://storage.blazingcdn.net/v3", {
+				headers: {
+					Authorization: `Bearer ${token}`,
+					accept: "application/json",
+				},
+				method: "GET",
+			});
+
+			if (response.ok) {
+				showMessage("Токен дійсний", "green");
+			} else {
+				showMessage("Токен недійсний", "red");
+			}
+		} catch (error) {
+			showMessage("Токен недійсний", "red");
+		}
 	};
 
-	// Авторизація
-	const authForm = document.getElementById('authForm');
-	authForm.addEventListener('submit', event => {
-		event.preventDefault();
-		const token = document.getElementById('token').value;
-
-		// Виконайте авторизацію тут і виведіть повідомлення відповідно
-		const headers = new Headers({
-			'Authorization': `Bearer ${token}`,
-			'accept': 'application/json'
-		});
-
-		fetch('https://storage.blazingcdn.net/v3', { method: 'GET', headers })
-			.then(response => {
-				if (response.ok) {
-					showMessage('Успішна авторизація', 'green');
-				} else {
-					showMessage('Помилка авторизації', 'red');
-				}
-			})
-			.catch(error => {
-				showMessage('Помилка авторизації', 'red');
+	const authorize = async (token) => {
+		try {
+			const response = await fetch("https://storage.blazingcdn.net/v3", {
+				headers: {
+					Authorization: `Bearer ${token}`,
+					accept: "application/json",
+				},
+				method: "GET",
 			});
+
+			if (response.ok) {
+				showMessage("Успішна авторизація", "green");
+			} else {
+				showMessage("Помилка авторизації", "red");
+			}
+		} catch (error) {
+			showMessage("Помилка авторизації", "red");
+		}
+	};
+
+	authForm.addEventListener("submit", (event) => {
+		event.preventDefault();
+		const token = tokenInput.value;
+		connectToServer();
+		checkTokenValidity(token);
+		authorize(token);
 	});
 });
